89dd7c73e618a53a665b0b13b7bd6a8b80e2bd0a
Yoav <yoavsbg@users.noreply.github.com>
2019-09-12 09:38:31 +0300

    Add

---
+++ PseudoLocalizationTool/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace PseudoLocalizationTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LengthInput = new System.Windows.Forms.Label();
+            this.LengthOutput = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.increamentText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 141);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 246);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(376, 141);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(249, 246);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Your Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(415, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pseudolocalize ";
+            // 
+            // LengthInput
+            // 
+            this.LengthInput.AutoSize = true;
+            this.LengthInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LengthInput.Location = new System.Drawing.Point(129, 410);
+            this.LengthInput.Name = "LengthInput";
+            this.LengthInput.Size = new System.Drawing.Size(24, 26);
+            this.LengthInput.TabIndex = 4;
+            this.LengthInput.Text = "0";
+            // 
+            // LengthOutput
+            // 
+            this.LengthOutput.AutoSize = true;
+            this.LengthOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LengthOutput.Location = new System.Drawing.Point(490, 410);
+            this.LengthOutput.Name = "LengthOutput";
+            this.LengthOutput.Size = new System.Drawing.Size(24, 26);
+            this.LengthOutput.TabIndex = 5;
+            this.LengthOutput.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(195, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Increase length by";
+            // 
+            // increamentText
+            // 
+            this.increamentText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.increamentText.Location = new System.Drawing.Point(390, 29);
+            this.increamentText.Name = "increamentText";
+            this.increamentText.Size = new System.Drawing.Size(44, 26);
+            this.increamentText.TabIndex = 7;
+            this.increamentText.Text = "30";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(440, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 525);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.increamentText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LengthOutput);
+            this.Controls.Add(this.LengthInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(690, 490);
+            this.Name = "Form1";
+            this.Text = "Pseudolocalize Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LengthInput;
+        private System.Windows.Forms.Label LengthOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox increamentText;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PseudoLocalizationTool/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PseudoLocalizationTool
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            StringBuilder pseudo = new StringBuilder();
+            int extra = Int32.Parse(increamentText.Text);
+            var length = Math.Ceiling(textBox1.Text.Length * (1 +(extra/100.0)));
+
+
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = r.Next(0, pseudoList.Count-1);
+                pseudo.Append(pseudoList[index]);
+            }
+
+            textBox2.Text = pseudo.ToString();
+
+            LengthInput.Text = textBox1.TextLength.ToString();
+            LengthOutput.Text = textBox2.TextLength.ToString();
+        }
+
+
+        private List<char> pseudoList = new List<char>
+        {
+            'á',
+            'β',
+            'ç',
+            'δ',
+            'è',
+            'ƒ',
+            'ϱ',
+            'λ',
+            'ï',
+            'J',
+            'ƙ',
+            'ℓ',
+            '₥',
+            'ñ',
+            'ô',
+            'ƥ',
+            '9',
+            'ř',
+            'ƨ',
+            'ƭ',
+            'ú',
+            'Ʋ',
+            'ω',
+            'ж',
+            '¥',
+            'ƺ',
+            'Â',
+            'ß',
+            'Ç',
+            'Ð',
+            'É',
+            'F',
+            'G',
+            'H',
+            'Ì',
+            'J',
+            'K',
+            '£',
+            'M',
+            'N',
+            'Ó',
+            'Þ',
+            'Q',
+            'R',
+            '§',
+            'T',
+            'Û',
+            'V',
+            'W',
+            'X',
+            'Ý',
+            'Z',
+        };
+
+
+
+
+
+
+        /*case 'a': out = 'á'; break;
+          case 'b': out = 'β'; break;
+          case 'c': out = 'ç'; break;
+          case 'd': out = 'δ'; break;
+          case 'e': out = 'è'; break;
+          case 'f': out = 'ƒ'; break;
+          case 'g': out = 'ϱ'; break;
+          case 'h': out = 'λ'; break;
+          case 'i': out = 'ï'; break;
+          case 'j': out = 'J'; break;
+          case 'k': out = 'ƙ'; break;
+          case 'l': out = 'ℓ'; break;
+          case 'm': out = '₥'; break;
+          case 'n': out = 'ñ'; break;
+          case 'o': out = 'ô'; break;
+          case 'p': out = 'ƥ'; break;
+          case 'q': out = '9'; break;
+          case 'r': out = 'ř'; break;
+          case 's': out = 'ƨ'; break;
+          case 't': out = 'ƭ'; break;
+          case 'u': out = 'ú'; break;
+          case 'v': out = 'Ʋ'; break;
+          case 'w': out = 'ω'; break;
+          case 'x': out = 'ж'; break;
+          case 'y': out = '¥'; break;
+          case 'z': out = 'ƺ'; break;
+          case 'A': out = 'Â'; break;
+          case 'B': out = 'ß'; break;
+          case 'C': out = 'Ç'; break;
+          case 'D': out = 'Ð'; break;
+          case 'E': out = 'É'; break;
+          case 'F': out = 'F'; break;
+          case 'G': out = 'G'; break;
+          case 'H': out = 'H'; break;
+          case 'I': out = 'Ì'; break;
+          case 'J': out = 'J'; break;
+          case 'K': out = 'K'; break;
+          case 'L': out = '£'; break;
+          case 'M': out = 'M'; break;
+          case 'N': out = 'N'; break;
+          case 'O': out = 'Ó'; break;
+          case 'P': out = 'Þ'; break;
+          case 'Q': out = 'Q'; break;
+          case 'R': out = 'R'; break;
+          case 'S': out = '§'; break;
+          case 'T': out = 'T'; break;
+          case 'U': out = 'Û'; break;
+          case 'V': out = 'V'; break;
+          case 'W': out = 'W'; break;
+          case 'X': out = 'X'; break;
+          case 'Y': out = 'Ý'; break;
+          case 'Z': out = 'Z'; break;*/
+    }
+}
